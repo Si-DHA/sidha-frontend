ce9e4e423fe44a32d217247beb63dd22b25d4c6e
Nur Fajar Sayyidul Ayyam <nur.fajar11@ui.ac.id>
2024-03-12 23:53:44 +0700

    fix: base url

--- src/pages/api/penawaran-harga-item/viewAllPenawaranHargaItem.ts
+++ src/pages/api/penawaran-harga-item/viewAllPenawaranHargaItem.ts
@@ -2,7 +2,7 @@ import { BASE_URL } from '@/app/constant/constant';
 
 export const viewAllPenawaranHargaItem = async (): Promise<any> => {
     try {
-        const response = await fetch('http://localhost:8080/api/penawaran-harga-item/id-penawaran-harga/view-all', {
+        const response = await fetch(BASE_URL+'/penawaran-harga-item/id-penawaran-harga/view-all', {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
