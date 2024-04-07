645635262d4f9032621887928c081ba8f9c474bf
Devina Hana Azhara <devina.hana@ui.ac.id>
2024-04-08 00:44:40 +0700

    Pbi 16 sistem pembayaran

---
+++ src/pages/api/pembayaran/viewBuktiImage.ts
@@ -0,0 +1,18 @@
+import { BASE_URL } from '@/app/constant/constant';
+
+export const viewBuktiImage = async (idInvoice: String, isPelunasan: boolean): Promise<any> => {
+    try {
+        const url = `${BASE_URL}/invoice/get-bukti?idInvoice=${idInvoice}&isPelunasan=${isPelunasan}`;
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+            }
+        });
+
+        return response.blob();
+    } catch (error: any) {
+        throw new Error(error.message);
+    }
+};
