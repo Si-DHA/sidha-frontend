86dac0d61f3c3a67b121633415e40d8506497448
Nur Fajar Sayyidul Ayyam <nur.fajar11@ui.ac.id>
2024-04-15 20:41:52 +0700

    fix: create order ui

--- src/pages/order/create/index.tsx
+++ src/pages/order/create/index.tsx
@@ -3,24 +3,15 @@ import Footer from "@/app/components/common/footer"
 import Cookies from 'js-cookie';
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import PurchaseOrderForm from "./PurchaseOrderForm";
-import { PurchaseOrder } from "@/pages/order/model";
+import { Order, OrderItem, Rute } from "../model";
+import { daftarRute, tipeBarang, tipeTruk } from "../data";
 
 const CreatePurchaseOrderPage = () => {
 
+    var userId = Cookies.get('userId');
     var isLoggedIn = Cookies.get('isLoggedIn');
     const [userRole, setUserRole] = useState('');
     const router = useRouter();
-    const [formData, setFormData] = useState([
-        {
-            id: '',
-            createdAt: '',
-            orderItems: [],
-            totalPrice: 0,
-            tanggalPengiriman: '',
-            klien: ''
-        } as PurchaseOrder
-    ] as PurchaseOrder[]);
 
     useEffect(() => {
         if (!isLoggedIn) {
@@ -30,27 +21,72 @@ const CreatePurchaseOrderPage = () => {
         setUserRole(role || '');
     },)
 
+    const [order, setOrder] = useState<Order>({
+        klienId: userId || '',
+        tanggalPengiriman: '',
+        orderItems: []
+    } as Order
+    );
 
-    const handleAddPurchaseOrder = () => {
+    const [formData, setFormData] = useState([
+        {
+            isPecahBelah: false,
+            tipeBarang: '',
+            tipeTruk: '',
+            keterangan: '',
+            rute: [
+                {
+                    source: '',
+                    destination: '',
+                    alamatPengiriman: '',
+                    alamatPenjemputan: ''
+                } as Rute
+            ]
+        } as OrderItem
+    ]);
+
+    const handleAddOrderItem = () => {
         setFormData([...formData, {
-            id: '',
-            createdAt: '',
-            orderItems: [],
-            totalPrice: 0,
-            tanggalPengiriman: '',
-            klien: ''
-        } as PurchaseOrder]);
+            isPecahBelah: false,
+            tipeBarang: '',
+            tipeTruk: '',
+            keterangan: '',
+            rute: [
+                {
+                    source: '',
+                    destination: '',
+                    alamatPengiriman: '',
+                    alamatPenjemputan: ''
+                } as Rute
+            ]
+        } as OrderItem
+        ]);
+    };
+
+    const handleRemoveOrderItem = (index: any) => {
+        if (formData.length > 1) {
+            setFormData(formData.filter((_, i) => i !== index));
+        }
     }
 
-    const BtnAddPurchaseOrder = () => {
-        return (
-            <div className="btn" onClick={handleAddPurchaseOrder}>Tambah Order Item</div>
-        );
+    const handleAddRute = (orderItemIndex: any) => {
+        const newFormData = [...formData];
+        newFormData[orderItemIndex].rute.push({
+            source: '',
+            destination: '',
+            alamatPengiriman: '',
+            alamatPenjemputan: ''
+        } as Rute);
+        setFormData(newFormData);
+    }
+
+    const handleRemoveRute = (ruteIndex: any, orderItemIndex: any) => {
+        const newFormData = [...formData];
+        newFormData[orderItemIndex].rute = newFormData[orderItemIndex].rute.filter((_, i) => i !== ruteIndex);
+        setFormData(newFormData);
     }
 
     const [errorDate, setErrorDate] = useState('');
-
-    // validasi tanggal pengiriman
     const handleDateChange = (e: any) => {
         const today = new Date();
         const selectedDate = new Date(e.target.value);
@@ -63,13 +99,101 @@ const CreatePurchaseOrderPage = () => {
         router.push('/order/checkout');
     }
 
+    const RuteOrderForm = ({ ruteIndex, orderItemIndex }: any) => {
+        return (
+            <div className="flex flex-col gap-4 w-full">
+
+                <div key={ruteIndex} className="flex gap-4">
+                    <label className="form-control flex-grow gap-2">
+                        <div className='flex flex-col'>
+                            {ruteIndex === 0 ? <div className="label">
+                                <span className="label-text">Rute Pengiriman</span>
+
+                            </div> : null}
+                            <select className="select select-bordered grow">
+                                <option disabled value="">Pilih satu</option>
+                                {daftarRute.map((rute, index) => (
+                                    <option key={index}>{rute[0]} - {rute[1]}</option>
+                                ))}
+                            </select>
+                        </div>
+                        <div className="flex flex-row gap-4 ">
+                            <input type="text" className="input input-bordered grow" placeholder="Alamat Penjemputan" />
+
+                            <input type="text" className="input input-bordered grow" placeholder="Alamat Pengiriman" />
+
+                        </div>
+                    </label>
+                    {ruteIndex > 0 && ( // Tombol hapus hanya ditampilkan untuk rute kedua dan seterusnya
+                        <div className="flex btn btn-error" onClick={() => handleRemoveRute(ruteIndex, orderItemIndex)}>Hapus Rute</div>
+                    )}
+                </div>
+
+                <div className="btn" onClick={() => handleAddRute(orderItemIndex)}>Tambah Rute</div>
+            </div>
+        );
+    };
+
+    const OrderItemForm = ({ index }: any) => {
+        return (
+            <div className="flex flex-col gap-2 justify-center items-center mih-h-screen p-8 border rounded-lg shadow-md">
+                {/* judul: item order ke-x */}
+                <div className="flex justify-between w-full">
+                    <span className="label-text font-bold">Item Order ke-{index + 1}</span>
+                </div>
+                <label className="form-control w-full">
+                    <div className="label">
+                        <span className="label-text">Tipe Barang</span>
+
+                    </div>
+                    <select required className="select select-bordered">
+                        <option disabled selected>Pilih satu</option>
+                        {tipeBarang.map((tipe, index) => (
+                            <option key={index}>{tipe}</option>
+                        ))}
+                    </select>
+                </label>
+                <div className="form-control w-full">
+                    <label className="label cursor-pointer">
+                        <span className="label-text">Barang rentan pecah belah (fragile)?</span>
+                        <input type="checkbox" defaultChecked className="checkbox checkbox-primary" />
+                    </label>
+                </div>
+                <label className="form-control w-full">
+                    <div className="label">
+                        <span className="label-text">Tipe Truk</span>
+
+                    </div>
+                    <select className="select select-bordered" required>
+                        <option disabled selected>Pilih satu</option>
+                        {tipeTruk.map((tipe, index) => (
+                            <option key={index}>{tipe}</option>
+                        ))}
+                    </select>
+                </label>
+                <label className="form-control w-full">
+                    <div className="label">
+                        <span className="label-text">Keterangan</span>
+                    </div>
+                    <textarea className="textarea textarea-bordered h-24" placeholder="Keterangan"></textarea>
+                </label>
+                {formData[index].rute.map((_, ruteIndex) => (
+                    <RuteOrderForm key={ruteIndex} ruteIndex={ruteIndex} orderItemIndex={index} />
+                ))}
+                {index > 0 && (
+                    <div className="btn btn-error w-full" onClick={() => handleRemoveOrderItem(index)}>Hapus Order Item</div>
+                )}
+            </div>
+        );
+    };
+
     return (
         <main className="flex min-h-screen flex-col items-center justify-between" data-theme="winter">
             <Drawer userRole={userRole}>
                 <div className="flex flex-col justify-center items-center mih-h-screen p-8">
                     <h1 className="text-3xl font-bold text-center ">Buat Purchase Order</h1>
                 </div>
-                <div className="flex flex-col gap-6 mx-4 my-4 " onSubmit={handleDetailOrder}>
+                <div className="flex flex-col gap-6 mx-4 my-4 " onSubmit={() => handleDetailOrder()}>
                     <form className="form-control flex flex-col gap-6">
 
                         <div className="flex flex-col w-full">
@@ -79,11 +203,10 @@ const CreatePurchaseOrderPage = () => {
                             <input type="date" required className="input input-bordered" onChange={handleDateChange} />
                             {errorDate && <div className="text-sm text-red-500">{errorDate}</div>}
                         </div>
-
-                        {[...Array(formData.length)].map((_, index) => (
-                            <PurchaseOrderForm key={index} index={index} />
+                        {formData.map((_, index) => (
+                            <OrderItemForm key={index} index={index} />
                         ))}
-                        <BtnAddPurchaseOrder />
+                        <div className="btn" onClick={handleAddOrderItem}>Tambah Order Item</div>
                         <button className="btn btn-primary" type="submit">Lihat Rincian Order</button>
                     </form>
                 </div>
