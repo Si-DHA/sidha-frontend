f1be06bf05fcc5e36e243572e2b82ff68490d93c
Devina <devina.hana@Devina-2.local>
2024-04-18 16:03:46 +0700

    style: change to bahasa

--- src/pages/pembayaran/index.tsx
+++ src/pages/pembayaran/index.tsx
@@ -136,11 +136,11 @@ const PembayaranPage = () => {
 
                         <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
                             <div className="modal-box">
-                                <h3 className="font-bold text-lg">Delete</h3>
-                                <p className="py-4">Are you sure you want to upload this?</p>
+                                <h3 className="font-bold text-lg">Unggah</h3>
+                                <p className="py-4">Apakah Anda yakin ingin mengunggah ini?</p>
                                 <div className="modal-action">
-                                    <button className="btn mr-2" onClick={() => document.getElementById('my_modal_5').close()}>Cancel</button>
-                                    <button className="btn btn-success" onClick={() => { uploadBuktiImage(); document.getElementById('my_modal_5').close(); }}>Upload</button>
+                                    <button className="btn mr-2" onClick={() => document.getElementById('my_modal_5').close()}>Batal</button>
+                                    <button className="btn btn-success" onClick={() => { uploadBuktiImage(); document.getElementById('my_modal_5').close(); }}>Unggah</button>
                                 </div>
 
                             </div>
