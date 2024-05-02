adfa1e11f7fb10a28ce5453ba664a80d425b2474
Devina <devina.hana@Devina-2.local>
2024-05-03 01:10:58 +0700

    feat: finish fe riwayat purchase order (sopir)

--- src/pages/insiden/karyawan/detail/[id].tsx
+++ src/pages/insiden/karyawan/detail/[id].tsx
@@ -2,12 +2,12 @@ import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { getInsidenById } from '@/pages/api/insiden/getInsidenById';
 import { updateStatus } from '@/pages/api/insiden/updateStatus';
-import Navbar from '@/app/components/common/navbar';
 import Footer from '@/app/components/common/footer';
 import SuccessAlert from '@/app/components/common/SuccessAlert';
 import FailAlert from '@/app/components/common/FailAlert';
 import { getBuktiFoto } from '@/pages/api/insiden/getBuktiFoto'
 import Drawer from "@/app/components/common/drawer";
+import Cookies from "js-cookie";
 
 const KaryawanInsidenDetailPage = () => {
     const router = useRouter();
@@ -16,6 +16,23 @@ const KaryawanInsidenDetailPage = () => {
     const [selectedStatus, setSelectedStatus] = useState('');
     const [alert, setAlert] = useState(null);
     const [buktiFotoUrl, setBuktiFotoUrl] = useState(null);
+    const [error, setError] = useState('');
+
+    var isLoggedIn = Cookies.get('isLoggedIn');
+    const [userRole, setUserRole] = useState('');
+  
+    useEffect(() => {
+      if (!isLoggedIn) {
+        router.push('/login');
+      }
+      const role = Cookies.get('role');
+      if (role === 'KARYAWAN') {
+        setUserRole(role);
+      } else {
+        setError('You are not allowed to access this page');
+      }
+  
+    }, [isLoggedIn, router])
 
     useEffect(() => {
         if (id) {
@@ -58,7 +75,7 @@ const KaryawanInsidenDetailPage = () => {
 
     return (
         <main className="flex flex-col items-center justify-between" data-theme="winter">
-            <Drawer userRole='userRole'>
+            <Drawer userRole={userRole}>
                 {alert}
                 <div className="bg-white shadow overflow-hidden sm:rounded-lg">
                     <div className="px-4 py-5 sm:px-6">
